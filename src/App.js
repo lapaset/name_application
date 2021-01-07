73187f7016e64a62a986789f24f527c874292666
llahti <llahti@student.hive.fi>
2021-01-07 17:16:14 +0200

    Create names list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import names from './names.json'
+import namesFromJson from './names.json'
 
 const App = () => {
 
-	console.log(names)
-	return (
-		<div>
-			<h1>Name App</h1>
+	const [names, setNames] = useState([])
 
+	useEffect(() => {
+		setNames(Object
+			.values(namesFromJson)[0]
+			.sort((a, b) => b.amount - a.amount))
+	}, [])
+
+	return (
+		<div className='main-container'>
+
+			<h1>Name App</h1>
+			
+			<ul>
+				{
+					names
+						.map(n => <li key={n.name}>
+							<span>{n.name}</span>
+							<span>{n.amount}</span>
+						</li>)
+				}
+			</ul>
+			
 		</div>
 	)
 }
